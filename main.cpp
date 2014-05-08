c509102c2de7cf951252f7469714f1f617be4f78
Carl Englund <englund92@gmail.com>
2014-05-08 09:15:45 +0200

    Made the mainfile ready for external GUI

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 #include "Field.h"
 #include "Gravity.h"
 #include "Wind.h"
-#include "objSystem.h"
+#include "ObjSystem.h"
 #include "SoapBubble.h"
 #include "Vortex.h"
 #include <iostream>
@@ -20,8 +20,20 @@ Object* gObject;
 SoapBubble* gBubble;
 
 
+sgct::SharedBool showStats(false);
+sgct::SharedBool showGraph(false);
+sgct::SharedDouble size_factor(0.5);
+sgct::SharedDouble curr_time(0.0);
+
+
 void initialize();
 void draw();
+void myPreSyncFun();
+void myPostSyncPreDrawFun();
+void myEncodeFun();
+void myDecodeFun();
+void externalControlCallback(const char * receivedChars, int size, int clientId);
+
 
 int main(int argc, char *argv[])
 {
@@ -31,6 +43,13 @@ int main(int argc, char *argv[])
 
 	gEngine->setInitOGLFunction(initialize);
 	gEngine->setDrawFunction(draw);
+	gEngine->setPreSyncFunction(myPreSyncFun);
+	gEngine->setPostSyncPreDrawFunction(myPostSyncPreDrawFun);
+	gEngine->setExternalControlCallback(externalControlCallback);
+
+	sgct::SharedData::instance()->setEncodeFunction(myEncodeFun);
+	sgct::SharedData::instance()->setDecodeFunction(myDecodeFun);
+
 
 	gParticles = new Snow(gEngine);
 	gWorld = new World(gEngine);
@@ -97,4 +116,67 @@ void draw()
 	gParticles->draw(delta);
 	
 	gObject->draw();
+}
+
+//Checking the time since the program started, not sure if we need this either.
+void myPreSyncFun()
+{
+	//Checks so the gEnginenode is actually the master.
+	if( gEngine->isMaster() )
+	{
+		//Sets the current time since the program started
+		curr_time.setVal(sgct::Engine::getTime());
+	}
+}
+
+//Shows stats and graph depending on if the variables are true or not. Dont know if we need this?
+void myPostSyncPreDrawFun()
+{
+	gEngine->setDisplayInfoVisibility(&showStats);
+	gEngine->setStatsGraphVisibility(&showGraph);
+}
+
+//Encodes the data sent from GUI
+void myEncodeFun()
+{
+	sgct::SharedData::instance()->writeDouble(&curr_time);
+	sgct::SharedData::instance()->writeDouble(&size_factor);
+	sgct::SharedData::instance()->writeBool(&showStats);
+	sgct::SharedData::instance()->writeBool(&showGraph);
+}
+
+//Decodes the data sent from GUI
+void myDecodeFun()
+{
+	sgct::SharedData::instance()->readDouble(&curr_time);
+	sgct::SharedData::instance()->readDouble(&size_factor);
+	sgct::SharedData::instance()->readBool(&showStats);
+	sgct::SharedData::instance()->readBool(&showGraph);
+}
+
+//Used to alter certain values when sent from GUI. This way we can alter the fields or change gravity in realtime! 
+void externalControlCallback(const char * receivedChars, int size, int clientId)
+{
+	//Checks so the gEnginenode is actually the master.
+	if(gEngine->isMaster())
+	{
+		//Compares the length of the strings so no weird runtime errors occur
+		if(size == 7 &&  strncmp(receivedChars, "stats", 5) == 0)
+		{
+			showStats.setVal(true);
+		}
+
+		else if(size == 7 &&  strncmp(receivedChars, "graph", 5) == 0)
+		{
+			showGraph.setVal(true);
+		}
+
+		else if(size >= 6 && strncmp(receivedChars, "size", 4) == 0)
+		{
+			//We need an int.
+			int tmpVal = atoi(receivedChars + 5);
+
+			size_factor.setVal(tmpVal);
+		}
+	}
 }
